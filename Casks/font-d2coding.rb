fc3ac1adabb5ad93cf197d78e6ad81af748c3193
Xvezda <xvezda@naver.com>
2022-12-09 02:07:07 +0900

    Add D2 Coding

---
+++ Casks/font-d2coding.rb
@@ -0,0 +1,11 @@
+cask "font-d2coding" do
+  version "1.3.2"
+  sha256 "0f1c9192eac7d56329dddc620f9f1666b707e9c8ed38fe1f988d0ae3e30b24e6"
+
+  url "https://github.com/naver/d2codingfont/releases/download/VER#{version}/D2Coding-Ver#{version}-20180524.zip"
+  name "D2 Coding"
+  homepage "https://github.com/naver/d2codingfont"
+
+  font "D2Coding/D2Coding-Ver#{version}-20180524.ttf"
+  font "D2Coding/D2CodingBold-Ver#{version}-20180524.ttf"
+end
