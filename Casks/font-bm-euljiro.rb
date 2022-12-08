5433f8d869a36e2e5eb013cf60a45f6252e07355
Xvezda <xvezda@naver.com>
2022-12-09 03:10:56 +0900

    Add BM Euljiro

---
+++ Casks/font-bm-euljiro.rb
@@ -0,0 +1,10 @@
+cask "font-bm-euljiro" do
+  version :latest
+  sha256 :no_check
+
+  url "https://woowahan-cdn.woowahan.com/static/fonts/BMEULJIRO.otf"
+  name "BM Euljiro"
+  homepage "https://www.woowahan.com/fonts"
+
+  font "BMEULJIRO.otf"
+end
