2c033b7d3a7f2af3c54121a7df032990f3517805
Xvezda <xvezda@naver.com>
2022-12-09 03:01:03 +0900

    Add BM Dohyeon

---
+++ Casks/font-bm-dohyeon.rb
@@ -0,0 +1,10 @@
+cask "font-bm-dohyeon" do
+  version :latest
+  sha256 :no_check
+
+  url "https://woowahan-cdn.woowahan.com/static/fonts/BMDOHYEON_otf.otf"
+  name "BM Dohyeon"
+  homepage "https://www.woowahan.com/fonts"
+
+  font "BMDOHYEON_otf.otf"
+end
