5ab9f94fc3e7604db46b3ad59a7ea49ad023d05f
Xvezda <xvezda@naver.com>
2022-12-09 03:07:14 +0900

    Add BM Hanna Pro

---
+++ Casks/font-bm-hanna-pro.rb
@@ -0,0 +1,10 @@
+cask "font-bm-hanna-pro" do
+  version :latest
+  sha256 :no_check
+
+  url "https://woowahan-cdn.woowahan.com/static/fonts/BMHANNAProOTF.otf"
+  name "BM Hanna Pro"
+  homepage "https://www.woowahan.com/fonts"
+
+  font "BMHANNAProOTF.otf"
+end
