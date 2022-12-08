10a7492472c564d5f8429982c2db111a18c00636
Xvezda <xvezda@naver.com>
2022-12-09 03:04:33 +0900

    Add BM Kirang Haerang

---
+++ Casks/font-bm-kirang-haerang.rb
@@ -0,0 +1,10 @@
+cask "font-bm-kirang-haerang" do
+  version :latest
+  sha256 :no_check
+
+  url "https://woowahan-cdn.woowahan.com/static/fonts/BMKIRANGHAERANG-OTF.otf"
+  name "BM Kirang Haerang"
+  homepage "https://www.woowahan.com/fonts"
+
+  font "BMKIRANGHAERANG-OTF.otf"
+end
