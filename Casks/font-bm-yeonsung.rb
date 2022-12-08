92889eba25b3dce755bcfcd2b1ac18e22d51dbba
Xvezda <xvezda@naver.com>
2022-12-09 03:04:21 +0900

    Add BM Yeonsung

---
+++ Casks/font-bm-yeonsung.rb
@@ -0,0 +1,10 @@
+cask "font-bm-yeonsung" do
+  version :latest
+  sha256 :no_check
+
+  url "https://woowahan-cdn.woowahan.com/static/fonts/BMYEONSUNG_otf.otf"
+  name "BM Yeonsung"
+  homepage "https://www.woowahan.com/fonts"
+
+  font "BMYEONSUNG_otf.otf"
+end
