e044987774d2e513c071969e9b05adcec39fa289
Xvezda <xvezda@naver.com>
2022-12-09 02:55:32 +0900

    Change BMJUA to BM Jua

--- Casks/font-bm-jua.rb
+++ Casks/font-bm-jua.rb
@@ -1,9 +1,9 @@
-cask "font-bmjua" do
+cask "font-bm-jua" do
   version :latest
   sha256 :no_check
 
   url "https://woowahan-cdn.woowahan.com/static/fonts/BMJUA_otf.otf"
-  name "BMJUA"
+  name "BM Jua"
   homepage "https://www.woowahan.com/fonts"
 
   font "BMJUA_otf.otf"
