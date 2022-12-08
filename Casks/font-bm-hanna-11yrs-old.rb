cdc7e75b75e032d7e61999ddbca668b70a6f9cde
Xvezda <xvezda@naver.com>
2022-12-09 02:59:22 +0900

    Add BM Hanna 11yrs old

---
+++ Casks/font-bm-hanna-11yrs-old.rb
@@ -0,0 +1,10 @@
+cask "font-bm-hanna-11yrs-old" do
+  version :latest
+  sha256 :no_check
+
+  url "https://woowahan-cdn.woowahan.com/static/fonts/BMHANNA_11yrs_otf.otf"
+  name "BM Hanna 11yrs Old"
+  homepage "https://www.woowahan.com/fonts"
+
+  font "BMHANNA_11yrs_otf.otf"
+end
