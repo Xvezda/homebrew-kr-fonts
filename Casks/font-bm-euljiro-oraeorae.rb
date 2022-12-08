2149c83e6a27938fc057e2f20e85421a9768e8eb
Xvezda <xvezda@naver.com>
2022-12-09 03:15:27 +0900

    Add BM Euljiro Oraeorae

---
+++ Casks/font-bm-euljiro-oraeorae.rb
@@ -0,0 +1,10 @@
+cask "font-bm-euljiro-oraeorae" do
+  version :latest
+  sha256 :no_check
+
+  url "https://woowahan-cdn.woowahan.com/static/fonts/BMEuljirooraeoraeOTF.otf"
+  name "BM Euljiro Oraeorae"
+  homepage "https://www.woowahan.com/fonts"
+
+  font "BMEuljirooraeoraeOTF.otf"
+end
