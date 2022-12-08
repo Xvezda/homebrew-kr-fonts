faa20fd100b7631ba729d2ade7e6a583bf3f1254
Xvezda <xvezda@naver.com>
2022-12-09 02:46:48 +0900

    Add BMJUA

---
+++ Casks/font-bmjua.rb
@@ -0,0 +1,10 @@
+cask "font-bmjua" do
+  version :latest
+  sha256 :no_check
+
+  url "https://woowahan-cdn.woowahan.com/static/fonts/BMJUA_otf.otf"
+  name "BMJUA"
+  homepage "https://www.woowahan.com/fonts"
+
+  font "BMJUA_otf.otf"
+end
