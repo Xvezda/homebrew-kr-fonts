ef9b1bae74c59fb24be889c6444a62f4b841179a
Xvezda <xvezda@naver.com>
2022-12-09 03:13:22 +0900

    Add BM Euljiro 10years Later

---
+++ Casks/font-bm-euljiro-10years-later.rb
@@ -0,0 +1,10 @@
+cask "font-bm-euljiro-10years-later" do
+  version :latest
+  sha256 :no_check
+
+  url "https://woowahan-cdn.woowahan.com/static/fonts/BMEuljiro10yearslaterOTF.otf"
+  name "BM Euljiro 10years Later"
+  homepage "https://www.woowahan.com/fonts"
+
+  font "BMEuljiro10yearslaterOTF.otf"
+end
