ab2b77df3f2dd1fd9caf4f2f6b6d21efb566990c
Xvezda <xvezda@naver.com>
2022-12-09 03:06:08 +0900

    Add BM Hanna Air

---
+++ Casks/font-bm-hanna-air.rb
@@ -0,0 +1,10 @@
+cask "font-bm-hanna-air" do
+  version :latest
+  sha256 :no_check
+
+  url "https://woowahan-cdn.woowahan.com/static/fonts/BMHANNAAir_otf.otf"
+  name "BM Hanna Air"
+  homepage "https://www.woowahan.com/fonts"
+
+  font "BMHANNAAir_otf.otf"
+end
