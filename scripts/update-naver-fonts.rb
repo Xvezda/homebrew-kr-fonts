3a5dae0cda5150fc5ba4b68105d988aed0f62d23
Xvezda <xvezda@naver.com>
2022-12-09 06:21:45 +0900

    Add naver fonts update script with clova implementation

---
+++ scripts/update-naver-fonts.rb
@@ -0,0 +1,64 @@
+require 'net/http'
+
+# TODO: implement nanum & maru
+
+def generate_clova_casks
+  uri = URI('https://hangeul.naver.com/font/clova')
+
+  html = Net::HTTP.get(uri)
+  scans = html.scan(/data-url="[^"]+\.ttf"/)
+
+  font_uris = scans.collect {|line| line.split('"')[1]}
+  font_filenames = font_uris.collect {|uri| uri.split('/')[-1]}
+
+  def filename_to_template(filename)
+    cask_name = filename
+      .gsub(/[A-Z]/, '-\&')
+      .gsub(/^-/, '')
+      .downcase
+      .gsub(/\.ttf$/, '')
+      .gsub(/-ce$/, '')
+
+    font_name = filename
+      .gsub(/[A-Z]/, ' \&')
+      .gsub(/\.ttf$/, '')
+      .strip
+
+    base_uri = "https://hangeul.naver.com/hangeul_static/webfont/clova"
+
+    template = {
+      "name" => "#{cask_name}.rb",
+      "source" => <<~EOF
+        cask "#{cask_name}" do
+          version :latest
+          sha256 :no_check
+
+          url "#{base_uri}/#{filename}"
+          name "#{font_name}"
+          homepage "https://hangeul.naver.com/font/clova"
+
+          font "#{filename}"
+        end
+      EOF
+    }
+  end
+
+  casks = font_filenames
+    .collect {
+      |filename| {
+        "filename" => filename,
+        "template" => filename_to_template(filename)
+      }
+    }
+end
+
+base_path = File.expand_path("../", File.dirname(__FILE__))
+
+casks = generate_clova_casks
+casks.each {|cask|
+  cask_filename = File.join(base_path, "Casks", cask["template"]["name"])
+  File.open(cask_filename, "w") {|f|
+    f.write(cask["template"]["source"])
+  }
+}
+
