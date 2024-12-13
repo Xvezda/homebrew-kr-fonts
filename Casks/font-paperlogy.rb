873ad302973254f4801ac5090d90966179a7b11c
Xvezda <xvezda@naver.com>
2024-12-13 15:29:20 +0900

    Add Paperlogy

---
+++ Casks/font-paperlogy.rb
@@ -0,0 +1,18 @@
+cask "font-paperlogy" do
+  version "1.001"
+  sha256 "6ffa5c8fc7539c61f419dcd2c4dd714556412f2455d26399e83792968c7b23d6"
+
+  url "https://github.com/Freesentation/paperlogy/raw/refs/heads/main/Paperlogy-#{version}.zip"
+  name "Paperlogy"
+  homepage "https://freesentation.blog/paperlogyfont"
+
+  font "Paperlogy-1Thin.ttf"
+  font "Paperlogy-2ExtraLight.ttf"
+  font "Paperlogy-3Light.ttf"
+  font "Paperlogy-4Regular.ttf"
+  font "Paperlogy-5Medium.ttf"
+  font "Paperlogy-6SemiBold.ttf"
+  font "Paperlogy-7Bold.ttf"
+  font "Paperlogy-8ExtraBold.ttf"
+  font "Paperlogy-9Black.ttf"
+end
